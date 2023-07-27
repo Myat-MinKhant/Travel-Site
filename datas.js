5d070cd394b1a43d11a29716919f5337b72e6424
Myat-MinKhant <125357767+Myat-MinKhant@users.noreply.github.com>
2023-07-27 22:38:11 +0630

    Add files via upload

---
+++ datas.js
@@ -0,0 +1,94 @@
+const Data = [
+  {
+    id: 1,
+    imgSrc: "images/img1.jpg",
+    destTitle: "Bora Bora",
+    location: "New Zeland",
+    grade: "CULTRUAL RELAX",
+    fees: "$700",
+    description:
+      " The epitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxurious stays and adventurours activities. ",
+  },
+  {
+    id: 2,
+    imgSrc: "images/img2.jpg",
+    destTitle: "Machu Picchu",
+    location: "Peru",
+    grade: "CULTRUAL RELAX",
+    fees: "$600",
+    description:
+      " Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the Sun Gate is simply spectacular. ",
+  },
+  {
+    id: 3,
+    imgSrc: "images/img3.jpg",
+    destTitle: "Great Barrier Reef",
+    location: "Australia",
+    grade: "CULTRUAL RELAX",
+    fees: "$700",
+    description:
+      " One of the most remarkable Australian natural gifts is the Great Barrier Reef. The hallmark off this place is 'lavish' and 'beauty'. Always interesting to be in this place. ",
+  },
+  {
+    id: 4,
+    imgSrc: "images/img4.jpg",
+    destTitle: "Cappadocia",
+    location: "Turkey",
+    grade: "CULTRUAL RELAX",
+    fees: "$800",
+    description:
+      " According to the World Tourism Ranking, 45 Million people visit Turkey each year, and from that about 2 Million come to visit Cappadocia. This place is known for its luxurious stays and adventurous activities. ",
+  },
+  {
+    id: 5,
+    imgSrc: "images/img5.webp",
+    destTitle: "Guanajuato",
+    location: "Mexico",
+    grade: "CULTRUAL RELAX",
+    fees: "$1100",
+    description:
+      " A city is central Mexico, Guanajuato is known for its history of silver mining and colonial architecture. The houses in the city are very attractively painted with the most bright colors available. Always welcome. ",
+  },
+  {
+    id: 6,
+    imgSrc: "images/img6.webp",
+    destTitle: "Cinque Terre",
+    location: "Italy",
+    grade: "CULTRUAL RELAX",
+    fees: "$840",
+    description:
+      " The vibrant hues of the houses are its benchmark and explain the beauty of this place. Also, if you are a foodie and love seafood, you will be exhilarated with the choice you are about to get here. Happy exploring great food! ",
+  },
+  {
+    id: 7,
+    imgSrc: "images/img7.webp",
+    destTitle: "Angkor Wat",
+    location: "Cambodia",
+    grade: "CULTRUAL RELAX",
+    fees: "$790",
+    description:
+      " Angkor Wat represents the entire range of Khmer art from the 9th to the 15th century. This temple is considered the heart and soul of Cambodia. This place is known for its luxurious stays and adventurous activities. ",
+  },
+  {
+    id: 8,
+    imgSrc: "images/img8.webp",
+    destTitle: "Taj Mahal",
+    location: "India",
+    grade: "CULTRUAL RELAX",
+    fees: "$900",
+    description:
+      " An immense mausoleum of white marble, built-in Agra by Mughal emperor Shah Jahan in memory of his wife Mumtaz, the monument is breathtakingly beautiful. This place is known for its luxurious stays and adventurous activities. ",
+  },
+  {
+    id: 9,
+    imgSrc: "images/img9.jpg",
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    grade: "CULTRUAL RELAX",
+    fees: "$500",
+    description:
+      " Bali is an Indonesian Island and one of the best holiday destinations in the Indonesian archipelago. Bali is known for its volcanic mountains, history, art & culture, food, temples and beautiful sandy beaches. ",
+  },
+];
+
+module.exports = Data;
